ed952edff430c92c741e940f0b39e0cf68bec310
limhyni <ghlduf114@gmail.com>
2021-12-22 17:13:55 +0900

    array 배열 for 문

--- main.cpp
+++ main.cpp
@@ -4,18 +4,28 @@ using namespace std;
 
 int main()
 {
-	cout << "HelloWorld" << endl;
+	//array배열 선언[n] = { 1,2,3,4,5,6......n }
+	int Sum = 0;
 
-	int Height = 0;
-	int Base = 0;
-	float Area = 0.0f;
+	//식을 구상하는 방법
+	//Sum = Sum +2
+	//Sum = Sum +3
+	//Sum = Sum +4
+	//Sum = Sum +5
+	//Sum = Sum +6
+	//....
+	//Sum = Sum +100
 
-	cin >> Height;
-	cin >> Base;
+	//노가다...식을 머리로 구상하고 함축(공통분모를 찾는다) 시킨다
 
-	Area = (static_cast<float>(Height) * static_cast<float>(Base)) / 2.0f;
+	for (int n = 1; n <= 100;++n)
+	{
+		Sum = Sum + n;
+	}
 
-	cout << Area << endl;
+
+
+	cout << sum << endl;
 
 	return 0;
 }
