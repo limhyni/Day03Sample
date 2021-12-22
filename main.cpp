cf4ffac1921ea6d9c3707bf6ab27e8ecd655d485
limhyni <ghlduf114@gmail.com>
2021-12-22 15:40:17 +0900

    triangle add

--- main.cpp
+++ main.cpp
@@ -6,5 +6,16 @@ int main()
 {
 	cout << "HelloWorld" << endl;
 
+	int Height = 0;
+	int Base = 0;
+	float Area = 0.0f;
+
+	cin >> Height;
+	cin >> Base;
+
+	Area = (static_cast<float>(Height) * static_cast<float>(Base)) / 2.0f;
+
+	cout << Area << endl;
+
 	return 0;
 }
